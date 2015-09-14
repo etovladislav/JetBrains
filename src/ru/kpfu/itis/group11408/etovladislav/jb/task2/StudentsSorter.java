90c5c8fef47cd41f65f6f00560033247796af709
etovladislav <etovladislav@yandex.ru>
2015-09-14 11:56:35 +0300

    Revert "jb"
    This reverts commit 8847d8fda23b23262c2061fd752517911e28666d.

--- src/ru/kpfu/itis/group11408/etovladislav/jb/task2/StudentsSorter.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.itis.group11408.etovladislav.jb.task2;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public interface StudentsSorter {
-	void process(InputStream input, OutputStream output);
-}
