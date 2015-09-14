9e105df8ba2134e362b774663d1e9ac686fd56fd
etovladislav <etovladislav@yandex.ru>
2015-09-14 12:19:18 +0300

    jb

---
+++ src/ru/kpfu/itis/group11408/etovladislav/jb/task2/StudentsSorter.java
@@ -0,0 +1,8 @@
+package ru.kpfu.itis.group11408.etovladislav.jb.task2;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface StudentsSorter {
+	void process(InputStream input, OutputStream output);
+}
